ae709012cb144181c6d2d17836c7878728acd763
Mohamed Mokhtar <mokh.first@gmail.com>
2023-04-28 12:11:33 +0300

    imagesSection

---
+++ app/eventDetails/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "@/components/EventDetails/HeroSection";
+
+const EventDetails = () => {
+  return (
+    <div className="w-full h-full">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default EventDetails;
--- components/Home/Favorites.tsx
+++ components/Home/Favorites.tsx
@@ -7,7 +7,16 @@ export default function Favorites() {
         <div className="relative isolate">
           <div className="overflow-hidden">
             <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
-              <div className="mx-auto gap-x-14 lg:mx-0 lg:max-w-none">
+              <div className="relative mx-auto gap-x-14 lg:mx-0 lg:max-w-none">
+                <img
+                  className="absolute right-[3%] top-[87px] w-[61%]"
+                  src="./beans.png"
+                ></img>
+                <img
+                  className="absolute -bottom-[37%] -left-[9.5rem] w-[75%]"
+                  src="./beanss.png"
+                ></img>
+
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="max-w-lg text-4xl font-bold leading-[4rem] text-gray-900 sm:text-5xl">
                     All your Favorites in one place
@@ -17,7 +26,7 @@ export default function Favorites() {
                     Heart And Have Them All In One Easy To Recall List.
                   </p>
                 </div>
-                <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-center sm:pl-20 lg:mt-0 lg:pl-0">
+                {/* <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-center sm:pl-20 lg:mt-0 lg:pl-0">
                   <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div className="relative">
                       <img
@@ -63,6 +72,28 @@ export default function Favorites() {
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
+                  </div>
+                </div> */}
+                <div className="flex items-center gap-x-32 pt-16">
+                  <div className="relative">
+                    <img
+                      src="./left.png"
+                      className="w-[400px] object-contain"
+                    ></img>
+                    <img
+                      src="./bottom.png"
+                      className="absolute -bottom-[20%] -right-[50%] z-10 w-[300px]"
+                    ></img>
+                  </div>
+                  <div className="relative">
+                    <img
+                      src="./right.png"
+                      className="w-[400px] object-contain"
+                    ></img>
+                    <img
+                      src="./up.png"
+                      className="absolute -right-[50%] -top-[20%] w-[300px]"
+                    ></img>
                   </div>
                 </div>
               </div>
